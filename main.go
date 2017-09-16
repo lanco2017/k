6a1c5b02ca2ac1ce7c3102c8ea21c331f5256920
lanco2017 <lanco2017@users.noreply.github.com>
2017-09-17 05:59:16 +0800

    Update main.go

--- main.go
+++ main.go
@@ -306,7 +306,7 @@ http://blog.kkcpct.org/#contact`
 
 					//this_week_text_1 :=`從缺，本週週報資料還沒送進來。`
 
-					ImageURL_week_1 := "https://trello-attachments.s3.amazonaws.com/5897c415be467e461197bfbd/59aafe4f341bd4e4fd52a89a/7a4e4c75c5cef1129b30557ef2676e1e/0903%E9%80%B1%E5%A0%B1%E9%A0%81%E5%B0%81%E9%9D%A2.jpg"
+					ImageURL_week_1 := "https://trello-attachments.s3.amazonaws.com/5897c415be467e461197bfbd/59bd14157729af4b436574f2/d1e005b4615a71a0eccaacf3bcfa3e6a/20170917.jpg"
 					obj_message_week_1 := linebot.NewImageMessage(ImageURL_week_1, ImageURL_week_1)
 
 					this_week_text_2 := `本週週報（2017/09/17）：
