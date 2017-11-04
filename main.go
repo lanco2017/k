15e37c79e6d3b7b72e29c34a72f6b275c1d5cc41
lanco2017 <oooolan.luckyoooo@gmail.com>
2017-11-05 02:07:42 +0800

    ++

--- main.go
+++ main.go
@@ -306,10 +306,10 @@ http://blog.kkcpct.org/#contact`
 
 					//this_week_text_1 :=`從缺，本週週報資料還沒送進來。`
 
-					ImageURL_week_1 := "https://trello-attachments.s3.amazonaws.com/5897c415be467e461197bfbd/59eb82223af55889fac552ab/46ec58399f22092578f3c76e2fa72fbf/20171022.index.jpg"
+					ImageURL_week_1 := "https://trello-attachments.s3.amazonaws.com/5897c415be467e461197bfbd/59fdfefff727e204462834ac/464163b341086a850bc4a74e9201574d/20171105index.jpg"
 					obj_message_week_1 := linebot.NewImageMessage(ImageURL_week_1, ImageURL_week_1)
 
-					this_week_text_2 := `本週週報（2017/10/22）：
+					this_week_text_2 := `本週週報（2017/11/05）：
 https://s.kkcpct.org/2gWFiQU
 
 近期週報收藏區：
